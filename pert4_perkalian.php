62ac2e4b1e0b1500e6edd574ba374eb714fd92c2
Iswanto007 <58384902+Iswanto007@users.noreply.github.com>
2021-03-20 14:06:34 +0700

    Add files via upload
    commit pertemuan 4

---
+++ pert4_perkalian.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="style.css">
+ <title>pertemuan 4</title>
+</head>
+<body>
+ <div class="section">
+  <div class="isi">
+  <h2>Tabel perkalian</h2>
+  <?php 
+  $i=12;
+  for($j=15; $j <= 45; $j=$j+2){
+    $k= $i*$j;
+    echo $i."x".$j."=".$k."<br>";
+  }
+ ?>
+  </div>
+ </div>
+</body>
+</html>
