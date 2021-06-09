1680302899f9aa63ee11329dc35098af661d1e49
iswanto <iswanto2424@gmail.com>
2021-06-09 19:40:41 +0700

    pert14_1

---
+++ pert14_latihan1.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Session</title>
+</head>
+<body>
+ <form action="" method="post">
+ <h2>Login here</h2>
+ <label for="">Username</label>
+ <input type="text" name="user">
+ <label for="">Password</label>
+ <input type="text" name="pass">
+ <input type="button" name="login" value="Log in">
+ </form>
+</body>
+</html>
